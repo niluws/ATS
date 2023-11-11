6044465894f2bb28cc60728797dff9aacdca622d
niluws <ns1378@yahoo.com>
2023-11-11 11:12:35 +0330

    UploadExcelAPIView

--- candidate/views.py
+++ candidate/views.py
@@ -8,7 +8,6 @@ from rest_framework.response import Response
 from .serializers import ExcelFileSerializer,CandidateSerializer
 from .models import ExcelFileModel,CandidateModel
 from authentication.permissions import IsSuperuserOrHR
-from django.db import transaction
 
 class UploadExcelAPIView(generics.CreateAPIView):
     serializer_class = ExcelFileSerializer
@@ -28,39 +27,38 @@ class UploadExcelAPIView(generics.CreateAPIView):
             workbook = load_workbook(file_path)
             worksheet = workbook.active
 
-            with transaction.atomic():
-                user_emails = set()
+            user_emails = set()
 
-                for row in worksheet.iter_rows(min_col=2, min_row=2):
-                    user_email = row[3].value
+            for row in worksheet.iter_rows(min_col=2, min_row=2):
+                user_email = row[3].value
 
-                    if user_email not in user_emails:
-                        user_emails.add(user_email)
-                        existing_candidate = CandidateModel.objects.filter(email=user_email).exists()
+                if user_email not in user_emails:
+                    user_emails.add(user_email)
+                    existing_candidate = CandidateModel.objects.filter(email=user_email).exists()
 
-                        if not existing_candidate:
-                            hyperlink = row[5].hyperlink
+                    if not existing_candidate:
+                        hyperlink = row[5].hyperlink
 
-                            if hyperlink is not None:
-                                response = requests.get(hyperlink.target)
-                                online_resume = BeautifulSoup(response.text, 'html.parser')
+                        if hyperlink is not None:
+                            response = requests.get(hyperlink.target)
+                            online_resume = BeautifulSoup(response.text, 'html.parser')
 
-                                try:
-                                    url = online_resume.find('a', {'class': 'btn btn-default'})['href']
-                                    resume_response = requests.get(url)
+                            try:
+                                url = online_resume.find('a', {'class': 'btn btn-default'})['href']
+                                resume_response = requests.get(url)
 
-                                    new_candidate = CandidateModel(
-                                        name=row[1].value,
-                                        job=row[0].value,
-                                        phone_number=row[2].value,
-                                        email=user_email,
-                                        request_date=row[4].value,
-                                        resume=ContentFile(resume_response.content, name=f"{uuid.uuid4()}.pdf"),
-                                    )
-                                    new_candidate.save()
-                                except (KeyError, requests.RequestException):
-                                    os.remove(file_path)
-                                    return Response({'success': False, 'status': 422, 'error': 'Cannot access URL'})
+                                new_candidate = CandidateModel(
+                                    name=row[1].value,
+                                    job=row[0].value,
+                                    phone_number=row[2].value,
+                                    email=user_email,
+                                    request_date=row[4].value,
+                                    resume=ContentFile(resume_response.content, name=f"{uuid.uuid4()}.pdf"),
+                                )
+                                new_candidate.save()
+                            except (KeyError, requests.RequestException):
+                                os.remove(file_path)
+                                return Response({'success': False, 'status': 422, 'error': 'Cannot access URL'})
 
         except Exception as e:
             return Response({'success': False, 'status': 500, 'error': f'Error processing file: {str(e)}'})

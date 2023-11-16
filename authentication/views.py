49530a677b31b53f4df629207c80680fb97c4441
niluws <ns1378@yahoo.com>
2023-11-16 07:38:19 +0100

    merge two methods

--- authentication/views.py
+++ authentication/views.py
@@ -55,50 +55,30 @@ def generate_and_send_otp(email,current_site):
 class RegisterAPIView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
 
-    
-    def perform_create(self, serializer):
-        
-        
-        try:
-            user = serializer.save()
-            user.profile = Profile.objects.create(user=user)
-            user.set_password(serializer.validated_data['password'])
-            user.save()
-            self.request.data['message'] ='registered'
-            self.request.data['user_id'] =user.pk
-
-            current_site = get_current_site(self.request)
-            generate_and_send_otp(user.email,current_site) 
-        except Exception as e:
-            return Response({'success': False, 'status': 400, 'error': e})
-        
     @log_user_activity
     def post(self, request, *args, **kwargs):
-        return super().post(request, *args, **kwargs)
+        try:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
 
+            user = serializer.save()
+            user.set_password(serializer.validated_data['password'])
+            user.save()
+            message={
+                    'message':'You registered successfuly',
+                    'data':serializer.data,
+                    
+                }
 
+            self.request.data['message'] = 'registered'
+            self.request.data['user_id'] = user.pk
 
-class VerifyAccountAPIView(views.APIView):
-    def get(self, request, otp_code):
-        r = redis.Redis(host='localhost', port=6379, db=0)
+            current_site = get_current_site(self.request)
+            generate_and_send_otp(user.email, current_site)
 
-        keys = r.keys('*')
-
-        for key in keys:
-            value = r.get(key)
-
-            if value.decode('utf-8') == otp_code:
-                user = User.objects.filter(email=key.decode('utf-8')).first()
-
-                if user:
-                    user.is_active = True
-                    user.save()
-                    r.delete(key)
-                    return Response({'success': True, 'status': 200, 'message': 'Account activated successfully'})
-                else:
-                    return Response({'success': False, 'status': 404, 'error': 'User not found for the provided email'})
-
-        return Response({'success': False, 'status': 404, 'error': 'Invalid OTP'})
+            return Response({'success': True, 'status': 201, 'message': message})
+        except Exception as e:
+            return Response({'success': False, 'status': 400, 'error': str(e)})
 
 
 class LoginAPIView(generics.CreateAPIView):
@@ -129,6 +109,24 @@ class LoginAPIView(generics.CreateAPIView):
             else:
                 return Response({'success': False, 'status': 401, 'error': 'Authentication failed'})
         else:return Response({'success': False, 'status': 401, 'error': 'Chack your email and verify your account'})
+
+
+
+class LogoutAPIView(generics.GenericAPIView):
+    serializer_class = LogoutSerializer
+
+    @log_user_activity
+    def get(self,request):
+            auth =jwt_manager.get_user_from_auth_header(self.request)
+            user = User.objects.get(email=auth)
+ 
+            if auth:
+                request.data['user_id'] = user.id
+                request.data['message'] = 'logged out'
+        
+                return Response({'success': True, 'status': 200, 'message': 'You logout successfuly'})
+            else:
+                return Response({'success': False, 'status': 401, 'error': 'User is not authenticated'})
 
 
 
@@ -171,22 +169,29 @@ class RefreshTokenAPIView(generics.CreateAPIView):
             return Response({'success': False, 'status': 401, 'error': 'Signature has expired'})
         except Exception :
             return Response({'success': False, 'status': 401, 'error': 'Invalid token'})
+      
        
+class VerifyAccountAPIView(views.APIView):
+    def get(self, request, otp_code):
+        r = redis.Redis(host='localhost', port=6379, db=0)
 
-class LogoutAPIView(generics.GenericAPIView):
-    serializer_class = LogoutSerializer
+        keys = r.keys('*')
 
-    @log_user_activity
-    def get(self,request):
-            auth =jwt_manager.get_user_from_auth_header(self.request)
-            user = User.objects.get(email=auth)
- 
-            if auth:
-                request.data['user_id'] = user.id
-                request.data['message'] = 'logged out'
-        
-                return Response({'success': True, 'status': 200, 'message': 'You logout successfuly'})
-            else:
-                return Response({'success': False, 'status': 401, 'error': 'User is not authenticated'})
+        for key in keys:
+            value = r.get(key)
+
+            if value.decode('utf-8') == otp_code:
+                user = User.objects.filter(email=key.decode('utf-8')).first()
+
+                if user:
+                    user.is_active = True
+                    user.save()
+                    r.delete(key)
+                    return Response({'success': True, 'status': 200, 'message': 'Account activated successfully'})
+                else:
+                    return Response({'success': False, 'status': 404, 'error': 'User not found for the provided email'})
+
+        return Response({'success': False, 'status': 404, 'error': 'Invalid OTP'})
+
 
 
